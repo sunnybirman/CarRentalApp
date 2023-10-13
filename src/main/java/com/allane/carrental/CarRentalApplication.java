bc4c30caeacae6bff97bd107e515d31c24bbd427
Sunny Birman <sunnybirman@gmail.com>
2023-10-14 00:43:12 +0200

    Added main application

--- src/main/java/com/allane/carrental/CarRentalApplication.java
+++ src/main/java/com/allane/carrental/CarRentalApplication.java
@@ -9,5 +9,12 @@ public class CarRentalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CarRentalApplication.class, args);
 	}
-
+//TODO Document Docker command -
+// docker run -d -p 3306:3306 --name mysql-container -e MYSQL_ROOT_PASSWORD=C@rR3nt@l2023 -e MYSQL_DATABASE=carrental_db mysql:latest
 }
+//TODO Remove
+/*
+* mysql -u root -p
+USE carrental_db;
+
+ * */
